1f9b714fba729bc62fa10e1279de5801e9ae9497
Noah Heldman <noah.heldman@gmail.com>
2010-12-31 15:21:44 -0800

    hoping to merge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
 
+<<<<<<< .merge_file_1RBm57
   # Return a title on a per-page basis
+=======
+  # Return a title on a per-page basis.
+>>>>>>> .merge_file_Nl0Ya5
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
@@ -9,5 +13,8 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+<<<<<<< .merge_file_1RBm57
 
+=======
+>>>>>>> .merge_file_Nl0Ya5
 end
