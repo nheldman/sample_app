6c42c0fdd7f51ea3864c52a45d1ac44d3d809686
Noah Heldman <noah.heldman@gmail.com>
2011-01-04 22:09:56 -0800

    removed smtp settings after installing heroku sendgrid

--- config/environments/env.rb
+++
@@ -1,8 +0,0 @@
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => 'noah.heldman',
-  :password             => 'pBKp@LWH*Vv49T$6',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
