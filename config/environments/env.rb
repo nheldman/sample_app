78aa4147cb6fc423542214a73b28a9402cbfcc8c
Noah Heldman <noah.heldman@gmail.com>
2011-01-04 21:54:55 -0800

    Removed domain from actionmailer smtp gmail config

--- config/environments/env.rb
+++ config/environments/env.rb
@@ -2,7 +2,6 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => 'baci.lindsaar.net',
   :user_name            => 'noah.heldman',
   :password             => 'pBKp@LWH*Vv49T$6',
   :authentication       => 'plain',
